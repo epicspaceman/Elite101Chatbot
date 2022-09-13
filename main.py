36413a1996362e8ef82c5ac1f261d32c599505cd
Robert Lockay <113408579+epicspaceman@users.noreply.github.com>
2022-09-13 23:17:05 +0000

    Added and tested age function.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import random
 import functions as fn
 
 generic_responses = ['That\'s so cool!', 'Interesting!', 'Really?']
-user_location = ''
-user_hobby = ''
+negative_responses = ['bad', 'disappointing', 'depressing', 'sucks', 'i hate it']
+positive_responses = ['good', 'i like it' ]
 user_name = input("Hello, what\'s your name? ")
 user_response = ''
 
-functions = [fn.location_question, fn.hobby_question]
+
+functions = [fn.location_question, fn.hobby_question, fn.user_age]
 
 while user_response != 'no':
-  random.choice(functions)(user_name, generic_responses)
+  random.choice(functions)(user_name, generic_responses, positive_responses, negative_responses)
   user_response = input("Would you like to continue our chat? ")
+
